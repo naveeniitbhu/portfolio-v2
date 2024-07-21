6c277e5847612993ac519c5baa29e43eee308910
naveengithub <ngnaven@gmail.com>
2024-07-21 14:47:46 +0530

    Update: Resume updated

--- src/app/ui/resume/index.js
+++ src/app/ui/resume/index.js
@@ -2,14 +2,13 @@ import ArrowLink from '@/app/ui/icons/arrowLink/ArrowLink';
 
 const Resume = () => {
   return (
-    <div className="mt-12">
-      <a className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 font-semibold text-slate-200 group/link text-base" href="/resume.pdf" target="_blank" rel="noreferrer noopener" aria-label="View Full Résumé (opens in a new tab)"><span>View Full <span className="inline-block">
+    <a className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 font-semibold text-slate-200 group/link text-base" href="https://drive.google.com/file/d/1PBsqhzdf4LB8kUuizss_JxL42PLSSiBJ/view?usp=drive_link" target="_blank" rel="noreferrer noopener" aria-label="View Full Resume (opens in a new tab)">
+      <span>View Full <span className="inline-block">
         Resume
         <ArrowLink />
       </span>
       </span>
-      </a>
-    </div>
+    </a>
   )
 }
 
